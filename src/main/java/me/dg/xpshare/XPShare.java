51173fc1f95014e6e7275ca69f2e4dacc645230c
Dg0222 <dg0222@hotmail.com>
2021-08-21 21:31:18 -0400

    Initial commit

---
+++ src/main/java/me/dg/xpshare/XPShare.java
@@ -0,0 +1,17 @@
+package me.dg.xpshare;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XPShare extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
