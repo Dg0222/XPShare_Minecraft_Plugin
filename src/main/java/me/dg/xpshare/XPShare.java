c449a4914e9d0bbd832f88759b53b887f1438c29
Daniel Garcia <43286355+Dg0222@users.noreply.github.com>
2021-08-22 15:08:34 -0400

    change text color on plugin disabled

--- src/main/java/me/dg/xpshare/XPShare.java
+++ src/main/java/me/dg/xpshare/XPShare.java
@@ -15,6 +15,6 @@ public final class XPShare extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[XPShare]: plugin is disabled!");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[XPShare]: plugin is disabled!");
     }
 }
